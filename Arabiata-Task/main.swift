58b9cafbaeb27d9ad0e7bb49b87d26bce5f308fc
Abdelrhman Eliwa <abdelrhmankamaleliwa@gmail.com>
2020-03-18 17:16:23 +0200

    Initial Commit

---
+++ Arabiata-Task/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Arabiata-Task
+//
+//  Created by Abdelrhman Eliwa on 3/18/20.
+//  Copyright © 2020 Abdelrhman Eliwa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
